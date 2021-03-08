9a9a6d66c978e080801b9696a0ea772de9bd0bff
Docchopper221 <chienyq221@gmail.com>
2021-03-08 16:34:35 +0800

    primer countdown + count down completed

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class GameState : MonoBehaviour
+{
+    private int gamestate = 0;
+    private Timer timer;
+    // Start is called before the first frame update
+    public float PrimerCountDown, GameCountDown;
+    public GameObject PrimerText, CountDownText;
+
+    void Start()
+    {
+        timer = this.GetComponent<Timer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gamestate == 0)
+        {
+            if (PrimerText != null && !PrimerText.Equals(null) )
+            {
+               
+                if (timer.getCountFinish())
+                {
+                    //Debug.Log("Current State =" + gamestate);
+                    setupTimer(PrimerText, PrimerCountDown);
+                    timer.starttimer();
+                    gamestate += 1;
+                }
+            }
+
+        }
+        if (gamestate == 1)
+        {
+            
+            if (PrimerText != null && !PrimerText.Equals(null) && timer.getCountFinish())
+                PrimerText.transform.gameObject.SetActive(false);
+            if (CountDownText != null && !CountDownText.Equals(null))
+            {
+                if (timer.getCountFinish())
+                {
+                    CountDownText.transform.parent.gameObject.SetActive(true);
+                    setupTimer(CountDownText, GameCountDown);
+                    timer.starttimer();
+                    gamestate += 1;
+                }
+            }
+        }
+        if(gamestate == 2 && timer.getCountFinish())
+        {
+            // de colour image
+            // play animation
+            // select colour and colouring
+            // submit button
+            Debug.Log("playing");
+        }
+    }
+
+    void setupTimer(GameObject text , float counter)
+    {
+        //Debug.Log(timer.getCountFinish());
+        timer.setText(text.GetComponent<TextMeshProUGUI>());
+        timer.setTimer(counter);
+    }
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,44 +7,88 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     //public variables
-    public float timer, fadespeed;
-    public bool StartTimer;
-    public GameObject Text;
+    public float fadespeed;
+
+    
 
     //private variables
     private TextMeshProUGUI Counter;
-    private float count;
+    private float count, timerInput,time;
+    private bool StartTimer;
+    //private GameObject Text;
     // Start is called before the first frame update
     void Start()
     {
-        Counter = Text.GetComponent<TextMeshProUGUI>();
-        Counter.text = Mathf.Round(timer).ToString();
-        count = timer;
+        //Counter = Text.GetComponent<TextMeshProUGUI>();
+        //Counter.text = Mathf.Round(timer).ToString();
+        //count = timer;
+        //StartTimer = false;
     }
+
+
+    //Coroutine method too fast.
+    /*public IEnumerator CountDown()
+    {
+        while(timer > 0)
+        {
+            yield return new WaitForSeconds(1);
+            displaytimer();
+            timer--; 
+        }
+        timer = 0;
+
+    }*/
+
 
     // Update is called once per frame
     void Update()
     {
+        time = Time.timeSinceLevelLoad;
+        //one off timer due to time.delta Time
         if (StartTimer)
-            if (timer > 0)
+            if (timerInput > 0)
             {
-                timer -= Time.deltaTime;
-                count = Mathf.Round(timer);
+                timerInput -= Time.deltaTime;
+                count = Mathf.Round(timerInput);
                 Counter.text = count.ToString();
-                if (count > timer)
+                if (count > timerInput)
                 {
                     Counter.color = Color.Lerp(Counter.color, Color.clear, fadespeed * Time.deltaTime);
                 }
                 else
                     Counter.color = Color.Lerp(Counter.color, Color.black, fadespeed * Time.deltaTime);
-
             }
             else
             {
-                timer = 0;
-                StartTimer = false;
-
+                timerInput = 0;
+                StartTimer = !StartTimer;
             }
-
     }
+
+    public void setTimer(float n)
+    {
+        this.timerInput = n;
+        this.displaytimer();
+    }
+    public void setText(TextMeshProUGUI text)
+    {
+        this.Counter = text;
+    }
+    public bool getCountFinish()
+    {
+        return (timerInput == 0);
+    }
+    public void starttimer()
+    {
+        this.StartTimer = true;
+    }
+    /*public void startcountdown()
+    {
+        StartCoroutine(CountDown());
+    }*/
+    private void displaytimer()
+    {
+       Counter.text = Mathf.Round(timerInput).ToString();
+    }
+
 }
